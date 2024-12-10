185251906a212d05dc48c522e997c1f70fab2424
Alexander Schenkel <aschenkel@kadenpartner.ch>
2024-12-10 20:35:54 +0100

    Revert "Day 10 Visualization using tcell"
    This reverts commit 72927ced2d997d0cf8f981c5b4cc9be7d516154a.

--- problems/day10/day.go
+++ problems/day10/day.go
@@ -2,11 +2,8 @@ package day10
 
 import (
 	"fmt"
-	"time"
 
 	"alexi.ch/aoc/2024/lib"
-	"github.com/bylexus/go-stdlib/eerr"
-	"github.com/gdamore/tcell/v2"
 )
 
 type Day10 struct {
@@ -46,53 +43,27 @@ func (d *Day10) Setup() {
 }
 
 func (d *Day10) SolveProblem1() {
-	s, err := tcell.NewScreen()
-	eerr.PanicOnErr(err)
-	err = s.Init()
-	eerr.PanicOnErr(err)
-
-	// Set default text style
-	defStyle := tcell.StyleDefault.Background(tcell.ColorDefault).Foreground(tcell.ColorDefault)
-	highlightStyle := tcell.StyleDefault.Background(tcell.ColorDefault).Foreground(tcell.ColorRed).Bold(true)
-	s.SetStyle(defStyle)
-	s.Clear()
-
-	d.drawMap(s, defStyle)
-	s.Show()
-
-	go func() {
-		d.s1 = 0
-		for startPoint := range d.trailHeads {
-			targetCoords := d.ramble(startPoint, s, highlightStyle)
-			// we only need unique end coords per start point, so create a set:
-			targetCoordSet := make(map[lib.Coord]int)
-			for _, coord := range targetCoords {
-				targetCoordSet[coord] += 1
-			}
-			d.s1 += len(targetCoordSet)
+	d.s1 = 0
+	for startPoint := range d.trailHeads {
+		targetCoords := d.ramble(startPoint)
+		// we only need unique end coords per start point, so create a set:
+		targetCoordSet := make(map[lib.Coord]int)
+		for _, coord := range targetCoords {
+			targetCoordSet[coord] += 1
 		}
-
-	}()
-
-	for {
-		ev := s.PollEvent()
-		switch ev.(type) {
-		case *tcell.EventKey:
-			s.Fini()
-			return
-		}
+		d.s1 += len(targetCoordSet)
 	}
-
 }
 
 func (d *Day10) SolveProblem2() {
 	d.s2 = 0
 	// Solution 2 in this case is even simpler than solution 1 :-))
 	// We do not need unique end coords, but count all reached end coords:
-	// for startPoint := range d.trailHeads {
-	// 	targetCoords := d.ramble(startPoint)
-	// 	d.s2 += len(targetCoords)
-	// }
+	for startPoint := range d.trailHeads {
+		targetCoords := d.ramble(startPoint)
+		// fmt.Printf("Start : %#v, reached ends: %#v, score: %d\n\n", startPoint, targetCoords, len(targetCoords))
+		d.s2 += len(targetCoords)
+	}
 }
 
 func (d *Day10) Solution1() string {
@@ -103,14 +74,9 @@ func (d *Day10) Solution2() string {
 	return fmt.Sprintf("%d", d.s2)
 }
 
-func (d *Day10) ramble(start lib.Coord, s tcell.Screen, style tcell.Style) []lib.Coord {
+func (d *Day10) ramble(start lib.Coord) []lib.Coord {
 	actNr := d.topologyMap[start.Y][start.X]
 	allTargetCoords := make([]lib.Coord, 0)
-
-	style = style.Foreground(tcell.NewRGBColor(int32((actNr+10)*12), int32(0), int32(0))).Background(tcell.NewRGBColor(int32(actNr*25), int32(actNr*25), int32(actNr*25)))
-	s.SetContent(start.X, start.Y, '*', nil, style)
-	s.Show()
-	time.Sleep(20 * time.Millisecond)
 
 	// recursive break condition: If we sit on a 9, we have reached
 	// our destination: we return the coords of that destination
@@ -125,22 +91,11 @@ func (d *Day10) ramble(start lib.Coord, s tcell.Screen, style tcell.Style) []lib
 		if nextCoord.X >= 0 && nextCoord.Y >= 0 && nextCoord.X < len(d.topologyMap[0]) && nextCoord.Y < len(d.topologyMap) {
 			nextNr := d.topologyMap[nextCoord.Y][nextCoord.X]
 			if actNr+1 == nextNr {
-				resultCoords := d.ramble(nextCoord, s, style)
+				resultCoords := d.ramble(nextCoord)
 				allTargetCoords = append(allTargetCoords, resultCoords...)
 			}
 		}
 	}
 
 	return allTargetCoords
-}
-
-func (d *Day10) drawMap(s tcell.Screen, style tcell.Style) {
-	s.SetContent(0, 0, 'A', nil, style)
-
-	for y, line := range d.topologyMap {
-		for x, nr := range line {
-			style = style.Foreground(tcell.NewRGBColor(int32(nr*25), int32(nr*25), int32(nr*25)))
-			s.SetContent(x, y, '█', nil, style)
-		}
-	}
 }
