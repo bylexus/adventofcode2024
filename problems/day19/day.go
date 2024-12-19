a8c2690795ace0b8efa8eb1e5f12eaa5544422bf
Alexander Schenkel <aschenkel@kadenpartner.ch>
2024-12-19 11:29:20 +0100

    Day 19 Part 2, and cleanup
    This was a really simple solution in the end. The trick was to memoize the already processed parts,
    otherwise the resultset will be too big.
    
    Also, a small optimization: I first filter the towels NOT containted in the fabric at all, so to save some rounds.

--- problems/day19/day.go
+++ problems/day19/day.go
@@ -2,7 +2,6 @@ package day19
 
 import (
 	"fmt"
-	"slices"
 	"strings"
 
 	"alexi.ch/aoc/2024/lib"
@@ -41,9 +40,6 @@ func (d *Day19) Setup() {
 func (d *Day19) SolveProblem1() {
 	d.s1 = 0
 
-	// results := make([][]string, 0)
-	// p := d.patterns[5]
-	// p := d.patterns[0]
 	for _, p := range d.patterns {
 		towels := make([]string, 0)
 		for _, towel := range d.towels {
@@ -51,30 +47,18 @@ func (d *Day19) SolveProblem1() {
 				towels = append(towels, towel)
 			}
 		}
-		slices.SortFunc(towels, func(i, j string) int {
-			return len(i) - len(j)
-		})
-		// fmt.Printf("pattern: %s, %#v\n", p, towels)
-		// memory := make(map[string][][]string)
-		// results := d.findTowels(p, memory, towels)
-		// if len(results) > 0 {
-		// 	d.s1++
-		// }
-		memory := make(map[string]bool)
-		results := d.hasMatch(p, memory, towels)
-		if results {
-			// fmt.Printf("Good pattern: %s\n", p)
+		memory := make(map[string]int)
+		results := d.countMatch(p, memory, towels)
+		if results > 0 {
 			d.s1++
-		} else {
-			// fmt.Printf("Bad pattern: %s\n", p)
+			d.s2 += results
 		}
-		// fmt.Printf("pattern: %s, %#v\n", p, results)
-		// fmt.Printf("len: %d\n", len(results))
 	}
 }
 
 func (d *Day19) SolveProblem2() {
-	d.s2 = 0
+	// already solved in part 1!
+
 }
 
 func (d *Day19) Solution1() string {
@@ -85,82 +69,29 @@ func (d *Day19) Solution2() string {
 	return fmt.Sprintf("%d", d.s2)
 }
 
-func (d *Day19) findTowels(pattern string, memory map[string][][]string, towels []string) [][]string {
-	// fmt.Printf("pattern: %s\n", pattern)
-	// if val, ok := memory[pattern]; ok {
-	// 	return val
-	// }
-	if len(pattern) == 0 {
-		return nil
-	}
-	newRes := make([][]string, 0)
-	found := false
-	for _, towel := range towels {
-		if len(pattern) < len(towel) {
-			continue
-		}
-		if strings.Index(pattern, towel) == 0 {
-			nextPattern := pattern[len(towel):]
-			var ret [][]string = nil
-			if len(nextPattern) > 0 {
-				ret = d.findTowels(nextPattern, memory, towels)
-				if ret == nil {
-					continue
-				}
-			}
-			found = true
-			for i, res := range ret {
-				if len(res) > 0 {
-					res = append([]string{towel}, res...)
-				} else {
-					res = []string{towel}
-				}
-				ret[i] = res
-			}
-			if len(ret) == 0 {
-				ret = [][]string{{towel}}
-			}
-			newRes = append(newRes, ret...)
-		}
-	}
-	// memory[pattern] = newRes
-	if found {
-		return newRes
-	} else {
-		return nil
-	}
-}
-
-func (d *Day19) hasMatch(pattern string, memory map[string]bool, towels []string) bool {
-	// fmt.Printf("pattern: %s\n", pattern)
+func (d *Day19) countMatch(pattern string, memory map[string]int, towels []string) int {
 	if val, ok := memory[pattern]; ok {
 		return val
 	}
-	if len(pattern) == 0 {
-		return false
-	}
-	// found := false
+	count := 0
 	for _, towel := range towels {
 		if len(pattern) < len(towel) {
 			continue
 		}
 		if strings.Index(pattern, towel) == 0 {
 			if len(pattern) == len(towel) {
-				memory[pattern] = true
-				return true
+				count++
+				continue
 			}
 			nextPattern := pattern[len(towel):]
-			// var ret bool = false
 			if len(nextPattern) > 0 {
-				ret := d.hasMatch(nextPattern, memory, towels)
-				if ret {
-					memory[pattern] = true
-					return true
+				ret := d.countMatch(nextPattern, memory, towels)
+				if ret > 0 {
+					count += ret
 				}
 			}
-			// return false
 		}
 	}
-	memory[pattern] = false
-	return false
+	memory[pattern] = count
+	return count
 }
